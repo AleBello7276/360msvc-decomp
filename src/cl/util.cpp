c631b9bc78d5d21d96c367a85e49fbc492ae971a
aleblbl <alell8632@gmail.com>
2026-08-17 01:45:55 +0200

    feat(cl) 3 matching functions in `util.cpp`

---
+++ src/cl/util.cpp
@@ -0,0 +1,105 @@
+#include "util.h"
+#include <cstdint>
+#include <cstring>
+#include <wchar.h>
+
+int strqlen(const wchar_t* s) {
+    UINT backslashes = 0;
+    UINT len = 0;
+    BOOL needs_quotes = FALSE;
+
+    if (*s != '\0') {
+        for (; *s != '\0'; s++) {
+            if (iswspace(*s)) {
+                needs_quotes = TRUE;
+            }
+
+            if (!(*s - '\"')) {
+                len += 1 + backslashes;
+            }
+
+            backslashes = (!(*s - '\\') ? backslashes + 1 : 0);
+
+            len++;
+        }
+
+        if (needs_quotes)
+            len += 2 + backslashes;
+    }
+
+    return len;
+}
+
+wchar_t* concat(wchar_t* dst, size_t src_len, const wchar_t* src);
+
+// NON_MATCHING -> fuck this, fusckit idk how many hours i spent on this alone, damn
+wchar_t* strqcpy(wchar_t* dst, const wchar_t* src) {
+    wchar_t* write = dst;
+    const wchar_t* in = src;
+    int backslashes = 0;
+    BOOL needs_quotes = false;
+
+    if (*src) {
+        for (; *in != L'\0'; in++) {
+            if (iswspace(*in))
+                needs_quotes = true;
+
+            if (!(*in - L'\"')) {
+                if (backslashes >= 0) {
+                    for (int i = (backslashes + 1) & 1; i != 0; i--) {
+                        *write++ = L'\\';
+                    }
+
+                    write += backslashes + 1;
+                    backslashes = 0;
+                }
+                backslashes--;
+            }
+
+            backslashes = (!(*in - '\\') ? backslashes + 1 : 0);
+
+            *write++ = *in;  // copy wchar, then advance
+        }
+
+        if (needs_quotes) {
+            size_t len = write - dst;
+            memmove(write + 1, write, sizeof(wchar_t) * len);
+            wchar_t* end = write + 1 + len;
+            *dst = L'\"';
+
+            if (backslashes >= 0) {
+                write = end;
+                for (int i = (backslashes) & 1; i != 0; i--) {
+                    *write++ = L'\\';
+                }
+                write += backslashes;
+            }
+
+            *end = L'\"';
+            write = end + 1;
+        }
+    }
+
+    *write = L'\0';
+    return dst;
+}
+
+wchar_t* append(wchar_t* dst, size_t src_len, const wchar_t* src);
+
+BOOL IsRunningUnderIDE(void**);
+
+BOOL IsOutputToConsole(HANDLE h) {
+    // is h char file
+    if ((GetFileType(h) & ~FILE_TYPE_REMOTE) != FILE_TYPE_CHAR)
+        return FALSE;
+
+    DWORD mode;
+    return GetConsoleMode(h, &mode) != FALSE;
+}
+
+wchar_t* gobblewhite(wchar_t* in) {
+    while (*in != L'\0' && iswspace(*in))
+        in++;
+
+    return in;
+}
--- src/cl/util.h
+++ src/cl/util.h
@@ -6,5 +6,18 @@
 
 #define CMD_ERR_0x1f67 0x1f67
 
+/*
+    strqlen (string quoted (!?!?) lenght)
+    length of wchars needed to write `s` as an escaped, optionally
+    quoted, Windows command-line argument.
+*/
+int strqlen(const wchar_t* s);
+
+/* check if compiler is running in visual studio */
+BOOL IsRunningUnderIDE(void**);
+
+/* skips white characters (i love this name lol) */
+wchar_t* gobblewhite(wchar_t* in);
+
 NORETURN void done(INT code);
 void cmderr(DWORD error, ...);
